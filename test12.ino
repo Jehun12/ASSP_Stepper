02f59d4ad71633831ee6a74d8ee51afe7703c902
Jehun12 <Jehun12@users.noreply.github.com>
2015-11-23 18:08:10 +0100

    New pinout and tests without serial
    Tests d'anti mongolisation :
    - Changer le pinout contre une éventuelle mongolisation de l'extreme -> nope
    - Tester sans liaison série qui pourrait ralentir le bignou -> nope
    - Changer les temps d'impulsion pour test stepping low et high speed-> nope
    
    Work in progress... >_<

--- test12.ino
+++ test12.ino
@@ -13,74 +13,74 @@
 //
 
 #define pinEnableX 25 // Activation du driver/pilote
-#define pinStepX    3 // Signal de PAS (avancement)
-#define pinDirX     2 // Direction
+#define pinStepX   2  // Signal de PAS (avancement)
+#define pinDirX    26 // Direction
 #define pinMS1X    22 // Bit 1 de selection du micro pas 
 #define pinMS2X    23 // Bit 2 de selection du micro pas
 #define pinMS3X    24 // Bit 3 de selection du micro pas 
 
 void setup()
 {
-  Serial.begin(9600);
-  Serial.println("Test A4988");
+  //Serial.begin(9600);
+  //Serial.println("Test A4988");
 
-  pinMode( pinEnableX, OUTPUT );
-  pinMode( pinStepX   , OUTPUT );
-  pinMode( pinDirX  , OUTPUT );
-  pinMode( pinMS1X  , OUTPUT );
-  pinMode( pinMS2X  , OUTPUT );
-  pinMode( pinMS3X  , OUTPUT );
+  pinMode(pinEnableX, OUTPUT);
+  pinMode(pinStepX, OUTPUT);
+  pinMode(pinDirX, OUTPUT);
+  pinMode(pinMS1X, OUTPUT);
+  pinMode(pinMS2X, OUTPUT);
+  pinMode(pinMS3X, OUTPUT);
 }
 
 void loop()
 {
   int i = 0;
   
-  digitalWrite( pinEnableX, LOW);  // Activation driver (logique inversée) 
-  digitalWrite( pinDirX   , HIGH); // Direction avant
-  digitalWrite( pinStepX  , LOW);  // Initialisation de la broche step
-  digitalWrite( pinMS1X  , LOW);  // Choix du micro pas (MS3..1 = 0 0 0 pour Full Step)
-  digitalWrite( pinMS2X  , LOW);
-  digitalWrite( pinMS3X  , LOW);
+  digitalWrite(pinEnableX, LOW);  // Activation driver (logique inversée) 
+  digitalWrite(pinDirX, HIGH); // Direction avant
+  digitalWrite(pinStepX, LOW);  // Initialisation de la broche step
+  digitalWrite(pinMS1X, HIGH);  // Choix du micro pas (MS3..1 = 0 0 0 pour Full Step)
+  digitalWrite(pinMS2X, LOW);
+  digitalWrite(pinMS3X, LOW);
   
-  // Avance de 200 pas
-  Serial.println("Tu tournes");
-  for( i=0; i < 200; i++)
+  // Avance d'un quart de tour (1 tour = 200 pas)
+  //Serial.println("Tu tournes");
+  for(i=0; i<20; i++)
   {
-    Serial.println( i );
-    digitalWrite( pinStepX, HIGH );
-    delay( 1 );
-    digitalWrite( pinStepX, LOW );
-    delay( 1 );
+    //Serial.println( i );
+    digitalWrite(pinStepX, HIGH);
+    delay(125);
+    digitalWrite(pinStepX, LOW);
+    delay(125);
   } 
 
   // Changer de direction
-  digitalWrite( pinDirX   , LOW); // Direction avant
+  digitalWrite(pinDirX, LOW); // Direction avant
   delay(2000);
 
-  // Refaire 200 pas dans l'autre sens
-  Serial.println("Tu reverse tournes");
-  for( i=0; i < 200; i++)
+  // Refaire un quart de tour dans l'autre sens
+  //Serial.println("Tu reverse tournes");
+  for(i=0; i<50; i++)
   {
-    Serial.println( i );
-    digitalWrite( pinStepX, HIGH );
-    delay( 10 );
-    digitalWrite( pinStepX, LOW );
-    delay( 10 );
+    //Serial.println( i );
+    digitalWrite(pinStepX, HIGH);
+    delay(10);
+    digitalWrite(pinStepX, LOW);
+    delay(10);
   } 
 
   // Pas de step et pas d'ordre... 
   //   l'axe du moteur est donc bloqué 
-  Serial.println("Axe bloquey + attendre 5 sec");
-  delay( 5000 );
+  //Serial.println("Axe bloquey + attendre 5 sec");
+  delay(5000);
 
   // déblocage de l'axe moteur
-  Serial.println("Deblocage axe");
-  digitalWrite( pinEnableX, HIGH ); // logique inversée
-
+  //Serial.println("Deblocage axe");
+  digitalWrite(pinEnableX, HIGH); // logique inversée
+  
   // Fin et blocage du programme
   // Presser reset pour recommander
-  Serial.println("Fin de programme");
+  //Serial.println("Fin de programme");
   while( true );
-}
 
+}
