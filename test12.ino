3270b473b270883ff8d1665b84b429d9e3eaae2a
Jehun12 <Jehun12@users.noreply.github.com>
2015-11-27 16:00:53 +0100

    Added Z Translation stepper motor
    ...And it works well !

--- test12.ino
+++ test12.ino
@@ -33,6 +33,14 @@
 #define pinEnableY 30 // Activation du driver/pilote
 #define pinDirY    31 // Direction
 
+// Pinout Moteur stepper-glissière pour translation selon Z (Seeed)
+#define pinStepZ   4  // Signal de PAS (avancement)
+#define pinMS1Z    32 // Bit 1 de selection du micro pas 
+#define pinMS2Z    33 // Bit 2 de selection du micro pas
+#define pinMS3Z    34 // Bit 3 de selection du micro pas 
+#define pinEnableZ 35 // Activation du driver/pilote
+#define pinDirZ    36 // Direction
+
 void setup()
 {
   Serial.begin(9600);
@@ -51,6 +59,13 @@ void setup()
   pinMode(pinMS1Y, OUTPUT);
   pinMode(pinMS2Y, OUTPUT);
   pinMode(pinMS3Y, OUTPUT);
+  
+  pinMode(pinEnableZ, OUTPUT);
+  pinMode(pinStepZ, OUTPUT);
+  pinMode(pinDirZ, OUTPUT);
+  pinMode(pinMS1Z, OUTPUT);
+  pinMode(pinMS2Z, OUTPUT);
+  pinMode(pinMS3Z, OUTPUT);
 }
 
 void loop()
@@ -59,20 +74,28 @@ void loop()
   
   //Init des sorties
   digitalWrite(pinEnableX, LOW);  // Activation driver (logique inversée) 
-  digitalWrite(pinDirX, HIGH); // Direction avant
+  digitalWrite(pinDirX, HIGH); // Dir = 1 <=> Sens trigonométrique
   digitalWrite(pinStepX, LOW);  // Initialisation de la broche step
-  digitalWrite(pinMS1X, LOW);  // Choix du micro pas (MS3..1 = 0 0 0 pour Full Step)
+  digitalWrite(pinMS1X, LOW);  // Choix du micro pas (MS1..3 = 0 0 0 pour Full Step)
   digitalWrite(pinMS2X, LOW);
   digitalWrite(pinMS3X, LOW);
   
   digitalWrite(pinEnableY, LOW);  // Activation driver (logique inversée) 
-  digitalWrite(pinDirY, HIGH); // Direction avant
+  digitalWrite(pinDirY, HIGH); // Dir = 1 <=> Sens trigonométrique
   digitalWrite(pinStepY, LOW);  // Initialisation de la broche step
-  digitalWrite(pinMS1Y, LOW);  // Choix du micro pas (MS3..1 = 0 0 0 pour Full Step)
+  digitalWrite(pinMS1Y, LOW);  // Choix du micro pas (MS1..3 = 0 0 0 pour Full Step)
   digitalWrite(pinMS2Y, LOW);
   digitalWrite(pinMS3Y, LOW);
   
-  // Avance d'un quart de tour (1 tour = 200 pas)
+  digitalWrite(pinEnableZ, LOW);  // Activation driver (logique inversée) 
+  digitalWrite(pinDirZ, HIGH); // DirZ = 1 <=> Descendre
+  digitalWrite(pinStepZ, LOW);  // Initialisation de la broche step
+  digitalWrite(pinMS1Z, LOW);  // Choix du micro pas (MS1..3 = 1 0 0 pour Half Step)
+  digitalWrite(pinMS2Z, LOW);
+  digitalWrite(pinMS3Z, LOW);
+  
+  
+  // Avance d'un quart de tour (1 tour = 200 pas) pour X et Y
   Serial.println("Tu tournes");
   for(i=0; i<50; i++)
   {
@@ -89,10 +112,10 @@ void loop()
   } 
 
   // Changer de direction
-  digitalWrite(pinDirX, LOW); // Direction avant
+  digitalWrite(pinDirX, LOW);
   delay(2000);
 
-  // Refaire un quart de tour dans l'autre sens
+  // Refaire un quart de tour dans l'autre sens pour X
   Serial.println("Tu reverse tournes");
   for(i=0; i<50; i++)
   {
@@ -104,13 +127,38 @@ void loop()
   } 
 
   // Pas de step et pas d'ordre... L'axe du moteur est donc bloqué 
-  Serial.println("Axe bloquey + attendre 5 sec");
+  Serial.println("Axes bloquey + attendre 5 sec");
   delay(5000);
 
-  //Déblocage de l'axe moteur
+  // Tests du moteur glissière Z
+  Serial.println("Tu glisses Z");
+  for(i=0; i<12; i++)
+  {
+    //Serial.println( i );
+    digitalWrite(pinStepZ, HIGH);
+    delay(1000);
+    digitalWrite(pinStepZ, LOW);
+    delay(1000);
+  }
+  
+  // Changement de sens
+  digitalWrite(pinDirZ, LOW);
+  Serial.println("Tu reverse glisses Z");
+  
+  for(i=0; i<12; i++)
+  {
+    //Serial.println( i );
+    digitalWrite(pinStepZ, HIGH);
+    delay(200);
+    digitalWrite(pinStepZ, LOW);
+    delay(200);
+  }
+ 
+  // Déblocage de l'axe moteur
   Serial.println("Deblocage axe");
   digitalWrite(pinEnableX, HIGH); // logique inversée
   digitalWrite(pinEnableY, HIGH); // logique inversée
+  digitalWrite(pinEnableZ, HIGH); // logique inversée
   
   // Fin et blocage du programme
   Serial.println("Fin de programme");
